50800a5cf116d260fbe985ae64dc7d6deb77cc97
Dorimo <thedorimo@gmail.com>
2021-02-16 11:17:32 +0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+/* Your Bot Default Prefix For Every Server */
+exports.Default_Prefix = "~";
+/* Your Bot All Embeds Color */
+exports.Color = "7DFF27";
+/* Your Bot Token (Secret) */
+exports.Token = "";
+/* Bot's Owner ID */
+exports.OwnerID = "774886672415326238";
